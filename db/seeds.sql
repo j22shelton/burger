15a9980215e9be61ced006479b4cba8b41e77a2e
JoJo <shelton.jocelyn@gmail.com>
2019-06-02 20:10:55 -0400

    database code

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Ham-Burgler', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon-Guacamole Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Kangaroo Burger', false);
